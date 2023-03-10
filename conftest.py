17184d3534eb9b74657b14837da22a53024ca6db
Chekanov Artem <tema-42@mail.ru>
2023-03-11 00:21:38 +0700

    удалил фикстуру, отредактировал оба теста

--- conftest.py
+++ conftest.py
@@ -4,8 +4,3 @@ from selene.support.shared import browser
 def set_size_browser_window():
         browser.config.window_width = 1024
         browser.config.window_height = 768
-
-
-@pytest.fixture()
-def google_find():
-    browser.open('https://google.com')
--- test_home_work.py
+++ test_home_work.py
@@ -3,10 +3,12 @@ from selene.support.shared import browser
 from selene import be, have
 
 
-def test_find_selene(set_size_browser_window, google_find):
+def test_find_selene(set_size_browser_window):
+    browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
-    browser.element('[id="search"]').should(have.text('selene полезная вещь'))
+    browser.element('[id="search"]').should(have.text('Yashaka/Selene Alternatives and Reviews (Jan 2023) - LibHunt'))
 
-def test_negative(set_size_browser_window, google_find):
-    browser.element('[name="q"]').should(be.blank).type('vdfbsghrerh').press_enter()
-    browser.element('[id="topstuff"]').should(have.text('По запросу vdfbsghrerh ничего не найдено.'))
+def test_negative(set_size_browser_window):
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('vсрабждо321dfbsghrerh').press_enter()
+    browser.element('[id="topstuff"]').should(have.text('По запросу vсрабждо321dfbsghrerh ничего не найдено.'))
